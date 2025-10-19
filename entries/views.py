90fefe094001a1bf2b481dd68270af2f74776f64
echiewe <126719218+echiewe@users.noreply.github.com>
2025-10-18 20:55:47 -0600

    Initial frontend: home, share, comments
    - css and javascript in template file for now

--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse, JsonResponse
+from .models import Entry
+from authors.models import Author
+from django.shortcuts import render
 
 def stream_home(request):
-	return HttpResponse("stream home (not implemented)")
+    entries = []
+    # IDs not being automatically set 
+    #new_entry = Entry(title='title', content='content', fqid='', author=author_obj)
+    entry_objs = Entry.objects.exclude(visibility='DELETED') 
+    # gets all the saved added entries from the database that AREN'T deleted
+    for entry in entry_objs:
+        entries.append({
+            "title": entry.title,
+            "content": entry.content,
+            "published": entry.published,
+            "author": entry.author,
+            "fqid": entry.fqid,
+            # "likeCount": entry.likeCount,
+            # 'commentCount': entry.commentCount.
+            # 'comments': entry.comments
+        })
+        # makes the list of pages to display in index.html
+    # TODO: sort the entries list so that most recent (including edited) show at the top
+    return render(request, "stream_home.html", { "entries": entries })
 
 def public_entries(request):
 	return HttpResponse("public entries (not implemented)")
