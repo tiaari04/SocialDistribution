8ebea26ce4d10c1abaa95502d3f0b8bcba63ace2
echiewe <126719218+echiewe@users.noreply.github.com>
2025-10-19 23:22:57 -0600

    Entries.views cleanup

--- entries/views.py
+++ entries/views.py
@@ -8,20 +8,8 @@ def stream_home(request, author_serial):
     entries = []
     entry_objs = Entry.objects.exclude(visibility='DELETED') 
     # gets all the saved added entries from the database that AREN'T deleted
-    for entry in entry_objs:
-        entries.append({
-            "title": entry.title,
-            "content": entry.content,
-            "published": entry.published,
-            "author": entry.author,
-            "fqid": entry.fqid,
-            "likeCount": entry.likes_count,
-            'serial': entry.serial,
-        })
-        # makes the list of pages to display in index.html
-	
     author = get_object_or_404(Author, serial=author_serial)
-    return render(request, "stream_home.html", { "entries": entries, "author" : author })
+    return render(request, "stream_home.html", { "entries": entry_objs, "author" : author })
 
 def public_entries(request):
 	return HttpResponse("public entries (not implemented)")
