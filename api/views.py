d7200fbbdf78e0dbcb7a0957cf5f90a781022b40
tiaari04 <tiawall04@gmail.com>
2025-11-19 11:57:51 -0700

    fix: Checking values from change

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from entries import services as entries_services
 from inbox import services as followers_services
 from inbox import serializers as followers_serializers
 from django.contrib.auth.models import User
+from codecs import decode
 
 # followers serializer will use variables to know how to format the data
 FOLLOWERS = 1
@@ -99,7 +100,8 @@ def api_author_following_detail(request, author_serial, foreign_encoded):
 	from authors.models import Author
 	print(f"Printed values: {author_serial} {foreign_encoded}")
 	author = get_object_or_404(Author, serial=author_serial)
-	actor_fqid = unquote(foreign_encoded).rstrip("/")
+	actor_fqid = decode(actor_fqid, 'unicode_escape')
+	actor_fqid = unquote(foreign_encoded)
 	print("Looking for:", repr(actor_fqid))
 	actor = get_object_or_404(Author, id=actor_fqid)
 	print("here")
