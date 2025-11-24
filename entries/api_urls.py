e8ead494e15c814d3f9118f6e49488400cc7259e
tiaari04 <tiawall04@gmail.com>
2025-11-24 12:29:31 -0700

    fix: Likes and comments API

--- entries/api_urls.py
+++ entries/api_urls.py
@@ -37,4 +37,42 @@ urlpatterns = [
         api_views.CommentLikesViewSet.as_view({"get": "list", "post": "create"}),
         name="api-entry-comment-likes",
     ),
+
+
+    # no slash
+    path(
+        "authors/<str:author_serial>/entries/<str:entry_serial>/comments/",
+        api_views.EntryCommentsViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-comments-no-slash",
+    ),
+    path(
+        "authors/<str:author_serial>/entries/<str:entry_serial>/likes/",
+        api_views.EntryLikesViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-likes-no-slash",
+    ),
+
+    # FQID based endpoints
+    path(
+        "entries/<path:entry_fqid>/comments/<path:comment_fqid>/likes/",
+        api_views.CommentLikesViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-comment-likes-fqid-no-slash",
+    ),
+    path(
+        "entries/<path:entry_fqid>/comments/",
+        api_views.EntryCommentsViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-comments-fqid-no-slash",
+    ),
+    path(
+        "entries/<path:entry_fqid>/likes/",
+        api_views.EntryLikesViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-likes-fqid-no-slash",
+    ),
+
+
+    # comment likes
+    path(
+        "authors/<str:author_serial>/entries/<str:entry_serial>/comments/<path:comment_fqid>/likes/",
+        api_views.CommentLikesViewSet.as_view({"get": "list", "post": "create"}),
+        name="api-entry-comment-likes-no-slash",
+    ),
 ]
